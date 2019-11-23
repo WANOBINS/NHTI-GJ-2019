75b5c189710e963edeb13b23eef3add7cdfe6b7a
zikiro <mrzombiekilla24@yahoo.com>
2019-11-23 16:33:47 -0500

    Rotation Script
    Rotating Square Script

---
+++ Assets/Scripts/Puzzles/Rotate.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float RotateAmount;
+    public float rotateTo;
+    public char Key;
+    public bool canRotate;
+    public int[] clearCode;
+    public int[] currentCode;
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(Key) && canRotate)
+        {
+            rotateTo += RotateAmount;
+            if(rotateTo >= 360)
+            {
+                rotateTo -= 360;
+            }
+            EnterCodeUp(0);
+        }
+
+
+
+        if (Mathf.Abs(Mathf.FloorToInt(transform.rotation.eulerAngles.y)) != rotateTo)
+        {
+            RotateObjectY(this.gameObject, rotateTo, 2f);
+            canRotate = false;
+        } else {
+            canRotate = true;
+        }
+
+
+    }
+    //GameObject obj, float rotation, float speed
+    public void RotateObjectX(GameObject obj, float rotation, float speed)
+    {
+        obj.transform.rotation = Quaternion.RotateTowards(obj.transform.rotation, Quaternion.Euler(rotation, 0.0f, 0.0f), speed);
+    }
+
+    //GameObject obj, float rotation, float speed
+    public void RotateObjectY(GameObject obj, float rotation, float speed)
+    {
+        obj.transform.rotation = Quaternion.RotateTowards(obj.transform.rotation, Quaternion.Euler(0.0f, rotation, 0.0f), speed);
+    }
+
+    public void EnterCodeUp(int pos)
+    {
+        currentCode[pos] += 1;
+        if (currentCode[pos] > 4)
+        {
+            currentCode[pos] = 1;
+        }
+    }
+
+}
